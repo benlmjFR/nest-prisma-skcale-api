38b4e61c9ae75d772c02900bc0b6d843cd682167
BENOITHAVETlmj <myungjae84@gmail.com>
2026-01-14 17:40:56 +0100

    add Swagger

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,22 +1,48 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsOptional,
+  IsString,
+  MinLength,
+  MaxLength,
+} from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({
+    example: 'john.doe@mail.com',
+    description: 'User email (unique)',
+  })
   @IsEmail()
   email!: string;
 
+  @ApiProperty({
+    example: 'StrongPassword123',
+    description: 'Hashed later using bcrypt',
+    minLength: 8,
+  })
   @IsString()
   @MinLength(8)
   password!: string;
 
+  @ApiPropertyOptional({
+    example: 'Dev fullstack',
+  })
   @IsOptional()
   @IsString()
+  @MaxLength(300)
   description?: string;
 
+  @ApiPropertyOptional({
+    example: '12 rue de Paris, 75000',
+  })
   @IsOptional()
   @IsString()
   address?: string;
 
+  @ApiPropertyOptional({
+    example: '+33000000000',
+  })
   @IsOptional()
   @IsString()
   phone?: string;
--- src/users/dto/update-users.dto.ts
+++ src/users/dto/update-users.dto.ts
@@ -1,25 +1,55 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
+// src/users/dto/update-user.dto.ts
+import {
+  IsEmail,
+  IsOptional,
+  IsString,
+  MinLength,
+  MaxLength,
+} from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class UpdateUserDto {
+  @ApiPropertyOptional({
+    example: 'new@mail.com',
+    description: 'New Email should be unique',
+  })
   @IsOptional()
   @IsEmail()
   email?: string;
 
+  @ApiPropertyOptional({
+    example: 'NewStrongPassword123',
+    description: 'The Password will be hashed later using bcrypt',
+    minLength: 8,
+  })
   @IsOptional()
   @IsString()
   @MinLength(8)
+  @MaxLength(100)
   password?: string;
 
+  @ApiPropertyOptional({
+    example: 'New description',
+  })
   @IsOptional()
   @IsString()
+  @MaxLength(255)
   description?: string;
 
+  @ApiPropertyOptional({
+    example: '12 rue de Lyon, 69000',
+  })
   @IsOptional()
   @IsString()
+  @MaxLength(255)
   address?: string;
 
+  @ApiPropertyOptional({
+    example: '+33698765432',
+  })
   @IsOptional()
   @IsString()
+  @MaxLength(20)
   phone?: string;
 }
